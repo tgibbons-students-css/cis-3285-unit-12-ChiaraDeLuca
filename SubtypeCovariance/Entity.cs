a4a88d17ce28916dcce2ae5463ee4737a2dcac2b
Chiara Deluca <cdeluca@css.edu>
2019-11-19 20:04:53 -0600

    Finished Entity

--- SubtypeCovariance/Entity.cs
+++ SubtypeCovariance/Entity.cs
@@ -8,11 +8,6 @@ namespace SubtypeCovariance
 {
     public class Entity
     {
-        public Entity(Guid newID)
-        {
-            ID = newID;
-        }
-
         public Guid ID { get; set; }
 
         public string Name { get; set; }
